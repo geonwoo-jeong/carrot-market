326243d570ba92330a986b33bd8c8b291452bc52
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2022-02-26 21:53:15 +0900

    Sending SMS

--- pages/api/users/enter.ts
+++ pages/api/users/enter.ts
@@ -1,29 +1,46 @@
+import twilio from 'twilio'
 import client from "@libs/server/client";
-import withHandler from "@libs/server/withHandler";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN)
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
     const { phone, email } = req.body
-    const payload = phone ? { phone: +phone } : { email }
-    
+    const user = phone ? { phone: +phone } : email? { email } : null
+
+    if (!user) return res.status(400).json({ ok: false })
+
+    const payload = Math.floor(100000 + Math.random() * 900000) + ""
+
     const token = await client.token.create({
         data: {
-            payload: '1234',
+            payload,
             user: {
                 connectOrCreate: {
                     where: {
-                        ...payload
+                        ...user
                     },
                     create: {
                         name: 'Anonymous',
-                        ...payload
+                        ...user
                     },
                 }
             }
         }
     })
+    if (phone) {
+        await twilioClient.messages.create({
+            messagingServiceSid: process.env.TWILIO_MSID,
+            to: phone,
+            body: `Your login token is ${payload}`
+        })    
+    }
 
-    return res.status(200).end()
+    return res.json({
+        ok: true
+    })
 }
 
 export default withHandler("POST", handler)
