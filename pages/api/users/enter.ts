0e17d2948a04435dfd26230d7be017b4d28ddb0f
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2022-02-26 21:32:53 +0900

    Token Logic

--- pages/api/users/enter.ts
+++ pages/api/users/enter.ts
@@ -5,15 +5,22 @@ import { NextApiRequest, NextApiResponse } from "next";
 async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { phone, email } = req.body
     const payload = phone ? { phone: +phone } : { email }
-    const user = await client.user.upsert({
-        where: {
-            ...payload
-        },
-        create: {
-            name: 'Anonymous',
-            ...payload
-        },
-        update: {}
+    
+    const token = await client.token.create({
+        data: {
+            payload: '1234',
+            user: {
+                connectOrCreate: {
+                    where: {
+                        ...payload
+                    },
+                    create: {
+                        name: 'Anonymous',
+                        ...payload
+                    },
+                }
+            }
+        }
     })
 
     return res.status(200).end()
