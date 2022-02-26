a8af5b09ffdc5fcf5723b7d673d9ab41f34015bd
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2022-02-26 20:47:11 +0900

    Paths

--- components/button.tsx
+++ components/button.tsx
@@ -1,9 +1,9 @@
-import { cls } from "../libs/utils";
+import { cls } from '@libs/client/utils'
 
 interface ButtonProps {
-  large?: boolean;
-  text: string;
-  [key: string]: any;
+  large?: boolean
+  text: string
+  [key: string]: any
 }
 
 export default function Button({
@@ -16,11 +16,11 @@ export default function Button({
     <button
       {...rest}
       className={cls(
-        "w-full bg-orange-500 hover:bg-orange-600 text-white  px-4 border border-transparent rounded-md shadow-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none",
-        large ? "py-3 text-base" : "py-2 text-sm "
+        'w-full bg-orange-500 hover:bg-orange-600 text-white  px-4 border border-transparent rounded-md shadow-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none',
+        large ? 'py-3 text-base' : 'py-2 text-sm '
       )}
     >
       {text}
     </button>
-  );
+  )
 }
--- components/message.tsx
+++ components/message.tsx
@@ -1,9 +1,9 @@
-import { cls } from "../libs/utils";
+import { cls } from '@libs/client/utils'
 
 interface MessageProps {
-  message: string;
-  reversed?: boolean;
-  avatarUrl?: string;
+  message: string
+  reversed?: boolean
+  avatarUrl?: string
 }
 
 export default function Message({
@@ -14,14 +14,14 @@ export default function Message({
   return (
     <div
       className={cls(
-        "flex  items-start space-x-2",
-        reversed ? "flex-row-reverse space-x-reverse" : ""
+        'flex  items-start space-x-2',
+        reversed ? 'flex-row-reverse space-x-reverse' : ''
       )}
     >
-      <div className="w-8 h-8 rounded-full bg-slate-400" />
-      <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+      <div className='w-8 h-8 rounded-full bg-slate-400' />
+      <div className='w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md'>
         <p>{message}</p>
       </div>
     </div>
-  );
+  )
 }
