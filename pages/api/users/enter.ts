47e1a06ab7c655427e1737829d38f177a4fa6d04
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2022-02-26 21:07:12 +0900

    Accounts Logic

--- pages/api/users/enter.ts
+++ pages/api/users/enter.ts
@@ -1,7 +1,21 @@
+import client from "@libs/server/client";
 import withHandler from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { phone, email } = req.body
+    const payload = phone ? { phone: +phone } : { email }
+    const user = await client.user.upsert({
+        where: {
+            ...payload
+        },
+        create: {
+            name: 'Anonymous',
+            ...payload
+        },
+        update: {}
+    })
+
     return res.status(200).end()
 }
 
